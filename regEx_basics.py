53cc6b7322db1642bb179f52f443531364f4e0f6
boodscode237 <boodscode@gmail.com>
2020-10-13 19:22:34 +0500

    finding a phone number with a function

---
+++ regEx_basics.py
@@ -0,0 +1,29 @@
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False  # not phone number-sized
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False  # no area code
+    if text[3] != '-':
+        return False  # missing dash
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False  # not first 3 digit
+    if text[7] != '-':
+        return False  # missing dash
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False  # not last 4 digit
+    return True
+
+
+res = isPhoneNumber('415-555-1234')
+print(res)
+
+
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
