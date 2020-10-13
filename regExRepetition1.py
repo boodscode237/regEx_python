bc16c83e273a2e8bf58736741e7c300c9d7ff021
boodscode237 <boodscode@gmail.com>
2020-10-13 20:59:09 +0500

    trying different types of regex

---
+++ regExRepetition1.py
@@ -0,0 +1,41 @@
+import re
+# Optional Matching with the Question Mark
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+mo1 = phoneRegex.search('My number is 415-555-4242')
+print(mo1.group())
+
+mo2 = phoneRegex.search('My number is 555-4242')
+print(mo2.group())
+
+
+# Matching Zero or More with the Star : The * (called the star or asterisk) means “match zero or more”
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The Adventures of Batman')
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
+
+
+# Matching One or More with the Plus: the + (or plus) means “match one or more.”
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo2.group())
+
+mo3 = batRegex.search('The Adventures of Batman')
+print(mo3 == None)
+
+# Matching Specific Repetitions with Braces: to repeat a specific number of times
+haRegex = re.compile(r'(Ha){3}')
+mo1 = haRegex.search('HaHaHa')
+print(mo1.group())
---
+++ regEx_repetition.py
@@ -0,0 +1,20 @@
+import re
+
+# Grouping with Parentheses
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print(mo.group(1))
+
+# Matching Multiple Groups with the Pipe
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+print(mo1.group())
+
+mo2 = heroRegex.search('Tina Fey and Batman')
+print(mo2.group())
+
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
